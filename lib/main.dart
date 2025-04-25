2dd17c145ff5f60ef6545d689cbdb11c5ef8c7c9
MoisesD19 <moisesdonayre19@gmail.com>
2025-04-24 23:56:01 -0500

    fix: fixed home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:eventwine/feature/register_and_login/presentation/pages/register_page.dart';
 import 'package:eventwine/feature/register_and_login/presentation/pages/login_page.dart';
 import 'package:eventwine/feature/home/presentation/pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es', null);
+
   runApp(const MyApp());
 }
 
@@ -13,12 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Galpon App',
+      title: 'EventWine',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: '/register',
+      initialRoute: '/home',
       routes: {
         '/login': (context) => const LoginPage(),
         '/register': (context) => RegisterPage(),
